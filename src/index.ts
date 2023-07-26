c9d6c9d0527ad550dc605cf023e3e6e1306fd1a6
Your Name <you@example.com>
2023-07-26 20:47:25 -0300

    teste 2

--- src/index.ts
+++ src/index.ts
@@ -1,120 +1,117 @@
-import express, { Request, Response } from "express"
-import dotenv from "dotenv"
-import { ModelRetorno, ModelAssinatura, ModelConfirmacaoEntrega } from "./models/setoresInterfaceInfra"
-import bodyParser from "body-parser"
-import { PrismaClient } from "@prisma/client"
-import axios, { AxiosRequestConfig } from "axios"
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import {
+  ModelRetorno,
+  ModelAssinatura,
+  ModelConfirmacaoEntrega,
+} from "./models/setoresInterfaceInfra";
+import bodyParser from "body-parser";
+import axios, { AxiosRequestConfig } from "axios";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
-const prisma = new PrismaClient()
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.post("/registrarAssinatura", async (req: Request, res: Response) => {
-    const requiscaoModelAss: ModelAssinatura = req.body
+  const requiscaoModelAss: ModelAssinatura = req.body;
 
-    //PROCESSO DB
-    const config: AxiosRequestConfig = {
-        method: "post",
-        url: process.env.URL_POST,
-        headers: {
-          "ngrok-skip-browser-warning": "69420",
-          "Content-Type": "application/json",
-        },
-        data: {
-            notaFiscal: Number(requiscaoModelAss.notaFiscal),
-            responsavel: requiscaoModelAss.responsavel,
-            assinatura_img: requiscaoModelAss.assinatura_img,            
-            setor: "assinatura"
-        },
-      };
+  //PROCESSO DB
+  const config: AxiosRequestConfig = {
+    method: "post",
+    url: process.env.URL_POST,
+    headers: {
+      "ngrok-skip-browser-warning": "69420",
+      "Content-Type": "application/json",
+    },
+    data: {
+      notaFiscal: Number(requiscaoModelAss.notaFiscal),
+      responsavel: requiscaoModelAss.responsavel,
+      assinatura_img: requiscaoModelAss.assinatura_img,
+      setor: "assinatura",
+    },
+  };
 
-      let response = await axios(config)
+  let response = await axios(config);
 
-      
-    if(response.status == 201 || response.status == 200) {
-        res.status(201).send({result: "Criado Com Sucesso"})
-    }else {
-        res.status(400).send({result: "Ocorreu algum erro"})
-    }
-})
+  if (response.status == 201 || response.status == 200) {
+    res.status(201).send({ result: "Criado Com Sucesso" });
+  } else {
+    res.status(400).send({ result: "Ocorreu algum erro" });
+  }
+});
 
 app.post("/registrarConfirmacao", async (req: Request, res: Response) => {
-    const requiscaoModelConfir: ModelConfirmacaoEntrega = req.body
+  const requiscaoModelConfir: ModelConfirmacaoEntrega = req.body;
 
-    if(req.body.sec != process.env.SEC) {
-        return res.status(400).send({result: "Algo falta no sistema"})
-    }else if (req.body.setor != "confirmacao") {
-        return res.status(400).send({result: "Setor Errado"})
-    }
+  if (req.body.sec != process.env.SEC) {
+    return res.status(400).send({ result: "Algo falta no sistema" });
+  } else if (req.body.setor != "confirmacao") {
+    return res.status(400).send({ result: "Setor Errado" });
+  }
 
-    //PROCESSO DB
-    const config: AxiosRequestConfig = {
-        method: "post",
-        url: process.env.URL_POST,
-        headers: {
-          "ngrok-skip-browser-warning": "69420",
-          "Content-Type": "application/json",
-        },
-        data: {
-            notaFiscal: requiscaoModelConfir.notaFiscal,
-            motorista: requiscaoModelConfir.motorista,
-            cidade: requiscaoModelConfir.cidade,
-            entregaConcluida: requiscaoModelConfir.entregaConcluida,
-            obs: requiscaoModelConfir.obs,
-            setor: "confirmacao entrega"
-        },
-      };
+  //PROCESSO DB
+  const config: AxiosRequestConfig = {
+    method: "post",
+    url: process.env.URL_POST,
+    headers: {
+      "ngrok-skip-browser-warning": "69420",
+      "Content-Type": "application/json",
+    },
+    data: {
+      notaFiscal: requiscaoModelConfir.notaFiscal,
+      motorista: requiscaoModelConfir.motorista,
+      cidade: requiscaoModelConfir.cidade,
+      entregaConcluida: requiscaoModelConfir.entregaConcluida,
+      obs: requiscaoModelConfir.obs,
+      setor: "confirmacao entrega",
+    },
+  };
 
-      let response = await axios(config)
+  let response = await axios(config);
 
-      
-    if(response.status == 201 || response.status == 200) {
-        res.status(201).send({result: "Criado Com Sucesso"})
-    }else {
-        res.status(400).send({result: "Ocorreu algum erro"})
-    }
-})
+  if (response.status == 201 || response.status == 200) {
+    res.status(201).send({ result: "Criado Com Sucesso" });
+  } else {
+    res.status(400).send({ result: "Ocorreu algum erro" });
+  }
+});
 
 app.post("/registrarRetorno", async (req: Request, res: Response) => {
-    const retorno: ModelRetorno = req.body
+  const retorno: ModelRetorno = req.body;
 
-    if(req.body.sec != process.env.SEC) {
-        return res.status(400).send({result: "Algo falta no sistema"})
-    }else if (req.body.setor != "retorno") {
-        return res.status(400).send({result: "Setor Errado"})
-    }
+  if (req.body.sec != process.env.SEC) {
+    return res.status(400).send({ result: "Algo falta no sistema" });
+  } else if (req.body.setor != "retorno") {
+    return res.status(400).send({ result: "Setor Errado" });
+  }
 
-    //PROCESSO DB
-    const config: AxiosRequestConfig = {
-        method: "post",
-        url: process.env.URL_POST,
-        headers: {
-          "ngrok-skip-browser-warning": "69420",
-          "Content-Type": "application/json",
-        },
-        data: {
-            notaFiscal:retorno.notaFiscal,
-            placa: retorno.placa,
-            hodometro: retorno.hodometro,
-            data: retorno.data,
-            obs: retorno.obs,
-            setor: "retorno"
-        },
-      };
+  //PROCESSO DB
+  const config: AxiosRequestConfig = {
+    method: "post",
+    url: process.env.URL_POST,
+    headers: {
+      "ngrok-skip-browser-warning": "69420",
+      "Content-Type": "application/json",
+    },
+    data: {
+      notaFiscal: retorno.notaFiscal,
+      placa: retorno.placa,
+      hodometro: retorno.hodometro,
+      data: retorno.data,
+      obs: retorno.obs,
+      setor: "retorno",
+    },
+  };
 
-      let response = await axios(config)
+  let response = await axios(config);
 
-      
-    if(response.status == 201 || response.status == 200) {
-        res.status(201).send({result: "Criado Com Sucesso"})
-    }else {
-        res.status(400).send({result: "Ocorreu algum erro"})
-    }
-})
+  if (response.status == 201 || response.status == 200) {
+    res.status(201).send({ result: "Criado Com Sucesso" });
+  } else {
+    res.status(400).send({ result: "Ocorreu algum erro" });
+  }
+});
 
-
-app.listen(process.env.PORT, () => console.log("Iniciou"))
+app.listen(process.env.PORT, () => console.log("Iniciou"));
